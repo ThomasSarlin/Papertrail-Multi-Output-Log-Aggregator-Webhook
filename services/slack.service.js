d1836dd5b85c6d3b958733f4f6e028959adc2d69
Thomas Sarlin <thomas.sarlin@gmail.com>
2023-01-21 21:29:27 +0100

    Removed console.log

--- services/slack.service.js
+++ services/slack.service.js
@@ -9,16 +9,13 @@ const SlackService = {
     init: () => {
         console.log(SLACKChannel, SLACKToken);
         if(!!SLACKToken && !!SLACKChannel){
-            console.log('Initializing slack integration');
             slackWebClient = new WebClient(SLACKToken);
         }
     },
     isAvailable: () => {
-        console.log(slackWebClient);
         return !!slackWebClient?.chat;
     },
     sendSlackMessage: (message) => {
-        console.log('sending slack message')
         return slackWebClient.chat.postMessage({
             text: message,
             channel: SLACKChannel,
